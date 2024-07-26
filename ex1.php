ddfdc6276db2fa174b423c7c14b7dfed2c71b624
Lauaxelis <axelgt3@gmail.com>
2024-07-26 14:52:29 +0200

    etape 1 et debut de 2

--- ex1.php
+++ ex1.php
@@ -1,1 +1,36 @@
-coucou
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+//1. Préparation des données :
+
+$ingrédients = ["2 Oignons", "2 gousses d'ail", "300 ml de passata ou coulis de tomate", "Mozzarella"];
+
+
+
+$actions = ["Coupez les oignons", "Découpez les gousses d'ail", "Ajoutez la passata ou la coulis de tomate", "Ajoutez la mozzarella", "Faites revenir les oignons et les gousses d'ail", "Mélangez tous les ingrédients"];
+
+//2. Demander à l’utilisateur
+
+$ingredient_supplementaire = readline("Veuillez saisir un ingrédient supplémentaire : ");
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
